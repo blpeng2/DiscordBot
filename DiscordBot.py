137a8f4c47c5840b4d4dbbe0c1e659a511907001
GNU_CS_UngSik <87903147+blpeng2@users.noreply.github.com>
2022-11-05 10:42:46 +0000

    feature/Title

--- DiscordBot.py
+++ DiscordBot.py
@@ -4,7 +4,7 @@ from discord import app_commands
 
 class Bot(discord.Client):
     def __init__(self):
-        super().__init__(intents=discord.Intents.default())
+        super().__init__(intents=discord.Intents.all())
         self.synced = False
 
     async def on_ready(self):
@@ -15,13 +15,51 @@ class Bot(discord.Client):
         print(f"we have logged in as {self.user}.")
 
 
+class Title():
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    async def addTitle(self, user: discord.Member, title):
+        await user.add_roles(title)
+
+    @classmethod
+    async def removeTitle(self, user: discord.Member, title):
+        await user.remove_roles(title)
+
+
 bot = Bot()
 tree = app_commands.CommandTree(bot)
 
 
-@tree.command(guild=discord.Object(id=1038138701961769021), name="test", description="testing")
-async def self(interaction: discord.Interaction, name: str):
+@tree.command(guild=discord.Object(id=1038138701961769021), name="생성", description="끝말잇기를 진행할 방을 생성합니다")
+async def _create(interaction: discord.Interaction, name: str):
     await interaction.response.send_message(f"I am working! {name}", ephemeral=True)
 
 
-bot.run("Token")
+# 매개변수를 lowercase로 작성하지 않으면 error 발생
+@tree.command(guild=discord.Object(id=1038138701961769021), name="칭호", description="칭호를 추가하거나 제거합니다")
+async def _title(interaction: discord.Interaction, username: str, title_name: str):
+    role = discord.utils.find(
+        lambda r: r.name == title_name, interaction.guild.roles)
+
+    user = discord.utils.find(
+        lambda m: m.name == username, interaction.guild.members)
+
+    # 칭호가 존재하지 않을 경우 예외처리
+    if role is None:
+        await interaction.response.send_message("칭호가 존재하지 않습니다", ephemeral=True)
+        return
+
+    if user is None:
+        await interaction.response.send_message("유저가 존재하지 않습니다.", ephemeral=True)
+
+    if role in user.roles:
+        await Title.removeTitle(user, role)
+        await interaction.response.send_message("칭호를 제거했습니다.", ephemeral=True)
+    else:
+        await Title.addTitle(user, role)
+        await interaction.response.send_message("칭호를 추가했습니다.", ephemeral=True)
+
+
+bot.run("OTE1MjA4NzMyMjM1OTU2Mjc2.GXEBVZ.48_F3LDywARuCJy9xc_4dcO_Kiz3qmwZ0nzc9E")
