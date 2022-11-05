81b62ff6d4b4b6bce0288881d02f87befbcbd5b8
GNU_CS_UngSik <87903147+blpeng2@users.noreply.github.com>
2022-11-05 14:36:54 +0000

    feature/Status

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,4 +1,5 @@
 import discord
+from pymongo import MongoClient
 from discord import app_commands
 
 
@@ -13,6 +14,54 @@ class Bot(discord.Client):
             await tree.sync(guild=discord.Object(id=1038138701961769021))
             self.synced = True
         print(f"we have logged in as {self.user}.")
+
+
+class Status():
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def getStatus(self, username: str) -> dict:
+        client = MongoClient(
+            "URL")
+
+        db = client["Discord"]["User"]
+        # TODO 아무도 찾지 못했을 경우 예외처리가 필요
+        user = db.find_one({"userName": username})
+        return user
+
+    @classmethod
+    def createStatus(self, post: dict):
+        client = MongoClient(
+            "URL")
+
+        db = client["Discord"]["User"]
+        # TODO 저장이 되었는지 확인하는 코드가 필요
+        db.insert_one(post)
+        return True
+
+    @classmethod
+    def refreshRanking(self):
+        client = MongoClient(
+            "URL")
+
+        db = client["Discord"]["User"]
+        users = []
+        for post in db.find():
+            users.append(
+                {
+                    "userId": post["userId"],
+                    "userName": post["userName"],
+                    "exp": post["exp"],
+                    "level": post["level"],
+                    "rank": post["rank"]
+                })
+
+        users.sort(key=lambda user: user["exp"], reverse=True)
+        for rank, user in enumerate(users):
+            user["rank"] = rank + 1
+            db.replace_one({"userName": user["userName"]}, user)
+        return True
 
 
 class Title():
@@ -30,6 +79,12 @@ class Title():
 
 bot = Bot()
 tree = app_commands.CommandTree(bot)
+
+
+@tree.command(guild=discord.Object(id=1038138701961769021), name="test", description="testing")
+async def _self(interaction: discord.Interaction):
+    Status.refreshRanking()
+    await interaction.response.send_message("complete")
 
 
 @tree.command(guild=discord.Object(id=1038138701961769021), name="생성", description="끝말잇기를 진행할 방을 생성합니다")
@@ -62,4 +117,15 @@ async def _title(interaction: discord.Interaction, username: str, title_name: st
         await interaction.response.send_message("칭호를 추가했습니다.", ephemeral=True)
 
 
-bot.run("Token")
+@tree.command(guild=discord.Object(id=1038138701961769021), name="경험치", description="유저의 경험치 상태와 랭킹을 확인합니다")
+async def _level(interaction: discord.Interaction, username: str):
+    if not discord.utils.find(lambda m: m.name == username, interaction.guild.members):
+        return
+    Status.refreshRanking()
+    user = Status.getStatus(username)
+
+    # TODO 예쁘게 꾸며서 메세지 보내도록 해야함
+    await interaction.response.send_message(user)
+
+
+bot.run("TOKEN")
